df0764b1193b8a22ae9bca6ce37b56a750196da0
Sathyan <86242815+sathyanmadhavan@users.noreply.github.com>
2022-12-22 19:43:29 +0530

    Delete dgvc.py

--- dgvc.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-driver=webdriver.Chrome("C:\chromedriver.exe")
-driver.get("https://easycollege.in/dgvcoe/college/stuindex.aspx")
-name=driver.find_element("xpath","//input[@type='text']")
-name.send_keys("1913101058139")
-name=driver.find_element("xpath","//input[@type='password']")
-name.send_keys("sat2525")
-name=driver.find_element("xpath","//input[@type='submit']")
-name.click()
