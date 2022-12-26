14eb9f98b9659b8d905d6472b603275b26156d28
sathyanmadhavan <sathyanmadhavan619@gmail.com>
2022-12-26 15:37:15 +0530

    Updated according to pylint conversion

--- test_fill_form_check.py
+++ test_fill_form_check.py
@@ -1,23 +1,26 @@
+"""
+This Script is used to test if the values entering properly
+"""
+#Importing time module
 import time
+#Importing webdriver from selenium
 from selenium import webdriver
-driver=webdriver.Chrome("C:\chromedriver.exe")
-driver.get("https://edumarshal.com/?gclid=CjwKCAiAnZCdBhBmEiwA8nDQxYzQa8u5bjYvZ8gMpm43uXkWXovhfRqJp7TaInW6ebpPpQi73XzhDhoCEhoQAvD_BwE")
-name=driver.find_element("xpath","//input[@name='input_1.3']")
-name.send_keys("Sathyan")
-name=driver.find_element("xpath","//input[@name='input_1.6']")
-name.send_keys("Madhavan")
-message=driver.find_element("xpath","//input[@type='email']")
-message.send_keys("xxx@gmail.com")
-phone=driver.find_element("xpath","//input[@type='tel']")
-phone.send_keys("1234567890")
-name=driver.find_element("xpath","//input[@name='input_5']")
-name.send_keys("Asan institute")
-city=driver.find_element("xpath","//input[@name='input_8']")
-city.send_keys("chennai")
-number=driver.find_element("xpath","//input[@name='input_7']")
-number.send_keys("10")
+#Initilaze driver instance
+driver=webdriver.Chrome()
+#.get method helps you to redirecrt to the URL
+driver.get("https://edumarshal.com/")
+#find_element method searches and locate the web element
+name=driver.find_element("xpath","//input[@name='input_1.3']").send_keys("Sathyan")
+name=driver.find_element("xpath","//input[@name='input_1.6']").send_keys("Madhavan")
+message=driver.find_element("xpath","//input[@type='email']").send_keys("xxx@gmail.com")
+#send_keys will send the value
+phone=driver.find_element("xpath","//input[@type='tel']").send_keys("1234567890")
+name=driver.find_element("xpath","//input[@name='input_5']").send_keys("Asan institute")
+city=driver.find_element("xpath","//input[@name='input_8']").send_keys("chennai")
+number=driver.find_element("xpath","//input[@name='input_7']").send_keys("10")
+#.click method will emulates a mouse click option
 check=driver.find_element("xpath","//label[@for='choice_1_6_1']").click()
-submit=driver.find_element("xpath","//input[@type='submit']")
-submit.click()
+submit=driver.find_element("xpath","//input[@type='submit']").click()
+#Sleep method will wait for the page to load
 time.sleep(3)
 driver.close()
